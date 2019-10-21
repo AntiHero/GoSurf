0a9e7418fedff4bed9fc10794ad5dc935b066706
AntiHero <prophreaker@gmail.com>
2019-10-21 15:31:05 +0300

    feat: full map animation add

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,28 @@ const city = document.querySelector('.header__location');
 const day = document.querySelector('.date__day');
 const monthAndyear = document.querySelector('.header__date span');
 
+/* Shore dots */
+const shoreDots = document.querySelectorAll('.shore-dot');
+const shoreDotWest = document.querySelector('.shore-dot--west');
+const shoreDotEast = document.querySelector('.shore-dot--east');
+const shoreDotSouth = document.querySelector('.shore-dot--south');
+const shoreDotNorth = document.querySelector('.shore-dot--north');
+
+/* Shore paths */
+const shorePaths = document.querySelectorAll('.shore-path');
+const shorePathEast = document.querySelector('.shore-path-east');
+const shorePathWest = document.querySelector('.shore-path-west');
+const shorePathSouth = document.querySelector('.shore-path-south');
+const shorePathNorth = document.querySelector('.shore-path-north');
+let shorePathSelected = document.querySelector('.shore-path--selected');
+
+/* Shore names */
+const shoreNames = document.querySelectorAll('.shore-name');
+const shoreNameWest = document.querySelector('.shore-name--west');
+const shoreNameEast = document.querySelector('.shore-name--east');
+const shoreNameSouth = document.querySelector('.shore-name--south');
+const shoreNameNorth = document.querySelector('.shore-name--north');
+
 $(function() {
   /* Geolocation */
   // navigator.geolocation.getCurrentPosition(function(position) {
@@ -45,21 +67,85 @@ $(function() {
   monthAndyear.innerHTML = m + ' | ' + y;
 
   $('.header__slider').slick({
-    infinite: false,
+    infinite: true,
     fade: true,
     speed: 400,
     prevArrow:
-      '<img class="header__slider-arrows arrow-left" src="img/arrow-left.svg" alt="" />',
+      '<img class="header__slider-arrows header__arrow-left" src="img/arrow-left.svg" alt="" />',
     nextArrow:
-      '<img class="header__slider-arrows arrow-right" src="img/arrow-right.svg" alt="" />',
+      '<img class="header__slider-arrows header__arrow-right" src="img/arrow-right.svg" alt="" />',
     asNavFor: '.header__slider-dots',
   });
 
   $('.header__slider-dots').slick({
-    infinite: false,
+    infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
     focusOnSelect: true,
   });
+
+  $('.header__slider-arrows')
+    .delay(3000)
+    .queue(function() {
+      $(this)
+        .css('visibility', 'visible')
+        .dequeue();
+    })
+    .fadeTo(500, 1, 'linear');
+
+  $('.header__slider-arrows').hover(function(e) {
+    $(this).css('opacity', e.type === 'mouseenter' ? '.7' : '1');
+  });
+
+  /* ---Shore animation--- */
+  $(shorePathSelected)
+    .delay(3000)
+    .animate(
+      {
+        'stroke-dashoffset': '0',
+      },
+      2500
+    );
+
+  /* Slide dots */
+  const sliderDots = document.querySelectorAll(
+    '.header__slider-dots .slick-slide'
+  );
+
+  /* Header arrows */
+  const arrows = document.querySelectorAll('.header__slider-arrows');
+  arrows[0].addEventListener('click', selectShore, false);
+  arrows[1].addEventListener('click', selectShore, false);
+
+  function selectShore(e) {
+    e.preventDefault();
+    $(shorePathSelected).stop().removeAttr('style');
+    sliderDots.forEach((slide, index) => {
+      if ($(slide).hasClass('slick-current')) {
+        shoreDots.forEach(dot => {
+          dot.classList.remove('shore-dot--selected');
+        });
+        shoreDots[index].classList.add('shore-dot--selected');
+
+        shoreNames.forEach(name => {
+          name.classList.remove('shore-name--selected', 'fadeIn');
+        });
+        shoreNames[index].classList.add('shore-name--selected');
+
+        shorePaths.forEach(path => {
+          path.classList.remove('shore-path--selected');
+        });
+
+        shorePaths[index].classList.add('shore-path--selected');
+        shorePathSelected = shorePaths[index];
+        $(shorePathSelected).delay(700).animate(
+          {
+            'stroke-dashoffset': '0',
+          },
+          2500
+        );
+      }
+    });
+  }
 });
